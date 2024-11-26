fc035dc4b9c33040a1b9faae36fb655aee8f3f41
Raghav Thakar <thakarr@oregonstate.edu>
2024-11-26 13:55:07 -0800

    read config in replay buffer

--- ReplayBuffer.py
+++ ReplayBuffer.py
@@ -1,10 +1,23 @@
 import random
 import numpy as np
+import yaml
 
 class ReplayBuffer:
-    def __init__(self, buff_size=10000):
+    def __init__(self, alg_config_filename):
+        self.config_filename = alg_config_filename
+        self._read_config()
         self.experiences = []
-        self.buff_size = buff_size
+    
+    def _read_config(self):
+        """Read and load ReplayBuffer configuration from the YAML file."""
+        with open(self.config_filename, 'r') as config_file:
+            self.config_data = yaml.safe_load(config_file)
+            print('[ReplayBuffer]: YAML config read.')
+
+        self._load_config()
+    
+    def _load_config(self):
+        self.buff_size = self.config_data["ReplayBuffer"]["buffer_size"]
     
     def add(self, transition, random_discard=True):
         if len(self.experiences) < self.buff_size:
